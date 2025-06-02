c92b4bdcdb70d229bc34b05a66cbc1129755e569
DerekPH4 <paoloyanes123@gmail.com>
2025-06-02 12:37:04 -0600

    Arreglo de puero en 0.0.0.0

--- app.py
+++ app.py
@@ -237,4 +237,7 @@ def actualizar_tabla(carpeta):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
